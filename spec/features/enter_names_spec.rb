ac0d9feacda74a2644a8aac608f2a29263b07c0d
unicornelia <ez.kornelia@gmail.com>
2017-01-10 20:19:18 +0000

    Refactoring specs for using web helpers to sign in

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'entering names' do
   scenario 'users can enter their names' do
-    visit '/'
-    fill_in :player1_name, with: 'A'
-    fill_in :player2_name, with: 'B'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'A vs. B'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,18 +2,12 @@ require 'spec_helper'
 
 feature 'view hitpoints' do
   scenario 'see player 2 hitpoints' do
-    visit '/'
-    fill_in :player1_name, with: 'A'
-    fill_in :player2_name, with: 'B'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'A: 100'
   end
 
   scenario 'see player 1 hitpoints' do
-    visit '/'
-    fill_in :player1_name, with: 'A'
-    fill_in :player2_name, with: 'B'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'B: 100'
   end
 end
